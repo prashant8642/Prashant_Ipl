429463f60dce4c46b936b648990e174fdfea7a6c
prashant8642 <67287287+prashant8642@users.noreply.github.com>
2020-07-21 13:00:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const extra=require("./matchesWon");
-const csvFilePath="../data.json";
+const csvFilePath="../deliveries.csv";
 csv()
 .fromFile(csvFilePath)
     .then((jsonobj)=>{console.log(jsonobj);
